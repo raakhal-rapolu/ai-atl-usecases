ae5844595c426398d8782e18fe98842e20473952
raakhal-rapolu <pandu3011@gmail.com>
2024-10-27 02:31:28 -0400

    updated Dockerfile

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
 # Set variables
-PROJECT_ID="<GCP_PROJECT_ID>"
+PROJECT_ID=$(gcloud config get-value project)
 REGION=us-central1
 SERVICE_NAME="usecase-svc"
+IMAGE="usecase-svc-image"
+REPOSITORY="usecase-svc-repo"
 
 # Authenticate with GCP
-gcloud auth configure-docker $REGION-docker.pkg.dev
+gcloud auth configure-docker $REGION-docker.pkg.dev -q
+
+# Create the Artifact Registry repository if it doesn't exist
+gcloud artifacts repositories create $REPOSITORY \
+  --repository-format=docker \
+  --location=$REGION \
+  --description="Docker repository for usecase service" || true
 
 # Build Docker image
-docker build -t $REGION-docker.pkg.dev/$PROJECT_ID/$SERVICE_NAME .
+docker build -t $REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE .
+
+# Tag Docker image
+docker tag $REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE $REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE:latest
 
 # Push Docker image to Google Artifact Registry
-docker push $REGION-docker.pkg.dev/$PROJECT_ID/$SERVICE_NAME
+docker push $REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE:latest
 
 # Deploy to Cloud Run
 gcloud run deploy $SERVICE_NAME \
-  --image $REGION-docker.pkg.dev/$PROJECT_ID/$SERVICE_NAME \
+  --image $REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE:latest \
   --platform managed \
   --region $REGION \
   --allow-unauthenticated \
-  --set-env-vars GEMINI_URL=$GEMINI_URL,ANTHROPIC_API_KEY=$ANTHROPIC_API_KEY
+
+echo "Deployment successful!"
